982f24924d659a939310e19024222a0557288dd1
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-31 21:41:15 +0700

    Update key.js

--- key.js
+++ key.js
@@ -5,17 +5,14 @@ const path = require('path');
 const correctKey = 'VREDEN 2025';
 
 // Fungsi untuk memeriksa key sebelum memulai bot
-async function checkKey() {
-    // Key yang disediakan oleh pengguna atau aplikasi
-    const userKey = 'VREDEN 2025';  // Ini bisa diganti dengan cara yang kamu inginkan untuk menerima key
-
+async function checkKey(userKey) {
     // Memeriksa apakah key yang diberikan sesuai dengan correctKey
     if (userKey === correctKey) {
         console.log('Key valid. Bot siap dijalankan!');
         return true; // Key valid
     } else {
-        console.log('Key tidak valid!');
-        return false; // Key tidak valid
+        console.log('Key tidak valid! Bot dihentikan.');
+        process.exit(); // Hentikan proses bot
     }
 }
 
