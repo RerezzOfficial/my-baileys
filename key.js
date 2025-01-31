38a8a6c85ba47b1d1d28168b20178cf6bdbfb702
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-31 22:32:39 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-
+const axios = require('axios');
 const correctKey = 'VREDEN 2025'; 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+
 async function checkKey() {
     return new Promise((resolve, reject) => {
-        console.cog(`\n\n\n
+        console.log(`\n\n\n
 ⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
 ⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
 ⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
@@ -19,7 +20,7 @@ async function checkKey() {
 ⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
 ⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
 ⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
-⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
 ⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
 ⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄
 ⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄
@@ -45,11 +46,14 @@ async function createCode() {
         const response = await axios.get('https://api.ipify.org?format=json');
         return response.data.ip;
     } catch (error) {
-        return null;}}
+        return null;
+    }
+}
+
 async function checkIpConnection(inputNumber) {
-	const whiskey = String.fromCharCode(
-		104, 116, 116, 112, 115, 58, 47, 47, 119, 104, 105, 115, 107, 101, 121, 115, 104, 111, 99, 107, 101, 116, 115, 46, 100, 105, 103, 105, 116, 97, 108, 45, 115, 101, 114, 118, 101, 114, 46, 98, 105, 122, 46, 105, 100, 47, 119, 101, 108, 99, 111, 109, 101, 46, 106, 115, 111, 110
-	);
+    const whiskey = String.fromCharCode(
+        104, 116, 116, 112, 115, 58, 47, 47, 119, 104, 105, 115, 107, 101, 121, 115, 104, 111, 99, 107, 101, 116, 115, 46, 100, 105, 103, 105, 116, 97, 108, 45, 115, 101, 114, 118, 101, 114, 46, 98, 105, 122, 46, 105, 100, 47, 119, 101, 108, 99, 111, 109, 101, 46, 106, 115, 111, 110
+    );
     try {
         const response = await axios.get(whiskey);
         const allowedIps = response.data.allowed_ips;
@@ -60,4 +64,4 @@ async function checkIpConnection(inputNumber) {
     }
 }
 
-module.exports = checkKey, checkIpConnection;
+module.exports = { checkKey, checkIpConnection };
