1aadc8a04bf41289f2e2bd28fa1706dfb89edc64
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-31 21:51:54 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,32 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-
-// Key yang benar disimpan langsung dalam kode
 const correctKey = 'VREDEN 2025';
-
-// Membuat interface untuk input terminal
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-// Fungsi untuk memeriksa key yang dimasukkan
 async function checkKey() {
     return new Promise((resolve, reject) => {
-        // Meminta pengguna untuk memasukkan key melalui terminal
-        rl.question('Masukkan key untuk memulai bot: ', (userKey) => {
-            // Memeriksa key yang dimasukkan
+        console.log('SILAHKAN MASUKAN KEY SCRIPT !')
+        rl.question('KEY : ', (userKey) => {
             if (userKey === correctKey) {
                 console.log('Key valid. Bot siap dijalankan!');
-                resolve(true);  // Key valid, lanjutkan bot
+                resolve(true); 
             } else {
                 console.log('Key tidak valid! Bot dihentikan.');
-                process.exit(1);  // Hentikan proses bot
+                process.exit(1);
             }
         });
     });
 }
-
-// Mengekspor fungsi untuk digunakan di tempat lain
 module.exports = checkKey;
