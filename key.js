28e333d3428ad526739d48edb9958aea0151baf0
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-31 21:20:11 +0700

    Create key.js

---
+++ key.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+// Key yang benar (disimpan langsung dalam kode)
+const correctKey = 'VREDEN 2025';
+
+// Fungsi untuk memeriksa key sebelum memulai bot
+function checkKey(keyFilePath) {
+    // Memeriksa apakah file key ada
+    if (fs.existsSync(keyFilePath)) {
+        const storedKey = fs.readFileSync(keyFilePath, 'utf8').trim();
+        // Memeriksa apakah key yang disimpan sesuai dengan key yang benar
+        if (storedKey === correctKey) {
+            console.log('Key valid. Bot siap dijalankan!');
+            return true; // Key valid
+        } else {
+            console.log('Key tidak valid!');
+            return false; // Key tidak valid
+        }
+    } else {
+        console.log('File key.txt tidak ditemukan!');
+        return false; // File key tidak ditemukan
+    }
+}
+
+// Mengekspor fungsi untuk digunakan di tempat lain
+module.exports = checkKey;
