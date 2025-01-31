63a66ea4d4c629e84eb5047e3b1b804d88a1f801
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-31 22:02:53 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const chalk = require('chalk');
-const correctKey = 'VREDEN 2025';
+const chalk = require('chalk'); 
+
+const correctKey = 'VREDEN 2025'; 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
 async function checkKey() {
     return new Promise((resolve, reject) => {
-        console.log(chalk.gren('SILAHKAN MASUKAN KEY SCRIPT !'))
+        console.log(chalk.green('SILAHKAN MASUKAN KEY SCRIPT !')); 
         rl.question('KEY : ', (userKey) => {
             if (userKey === correctKey) {
-                console.log(chalk.gren('KEY BENAR BOT DI JALANKAN !'));
+                console.log(chalk.green('KEY BENAR BOT DI JALANKAN !'));  
                 resolve(true); 
             } else {
-                console.log(chalk.red('KEY ANDA TIDAK VALID !!!'));
-                process.exit(1);
+                console.log(chalk.red('KEY ANDA TIDAK VALID !!!')); 
+                process.exit(1); 
             }
         });
     });
 }
+
 module.exports = checkKey;
