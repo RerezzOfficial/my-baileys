fe49fca6175f9e764341bcfe36daf0a9819340d0
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-31 21:56:38 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const chalk = require('chalk');
 const correctKey = 'VREDEN 2025';
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,13 +9,13 @@ const rl = readline.createInterface({
 });
 async function checkKey() {
     return new Promise((resolve, reject) => {
-        console.log('SILAHKAN MASUKAN KEY SCRIPT !')
+        console.log{chalk.gren('SILAHKAN MASUKAN KEY SCRIPT !'))
         rl.question('KEY : ', (userKey) => {
             if (userKey === correctKey) {
-                console.log('Key valid. Bot siap dijalankan!');
+                console.log(chalk.gren('KEY BENAR BOT DI JALANKAN !')};
                 resolve(true); 
             } else {
-                console.log('Key tidak valid! Bot dihentikan.');
+                console.log{chalk.red('KEY ANDA TIDAK VALID !!!'));
                 process.exit(1);
             }
         });
